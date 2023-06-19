dedc9a2733211324ffc53ee9199ee0c262a1b4f3
Legacynnn <danmelomour@gmail.com>
2023-06-18 23:50:51 -0300

    feat: SessionProvider on client side

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,10 +1,15 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { SessionProvider } from "next-auth/react";
 import React from "react";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const queryClient = new QueryClient()
-  
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider>{children}</SessionProvider>
+    </QueryClientProvider>
+  );
 }
